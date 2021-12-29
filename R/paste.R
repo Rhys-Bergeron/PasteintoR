50918f9db7b4e869a9c2db77bc9a07070a0792ed
Rhys Bergeron <63369818+Rhys-Bergeron@users.noreply.github.com>
2021-12-29 13:30:48 -0500

    Update paste.R

--- R/paste.R
+++ R/paste.R
@@ -1,4 +1,4 @@
 paste_into_R <- function() {
-  code_to_execute <-  "df_xlsx <- read.table(\"clipboard\", sep = \"\\t\", header = TRUE)"
+  code_to_execute <-  "df_clipboard <- read.table(\"clipboard\", sep = \"\\t\", header = TRUE)"
   rstudioapi::sendToConsole(code = code_to_execute, execute = TRUE, echo = TRUE)
 }
