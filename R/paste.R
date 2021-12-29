fc310a8cd20c3937453f48636a9a5b2f2d2bda4a
Rhys Bergeron <63369818+Rhys-Bergeron@users.noreply.github.com>
2021-12-29 11:42:41 -0500

    Update paste.R

--- R/paste.R
+++ R/paste.R
@@ -1,4 +1,4 @@
 paste_into_R <- function() {
-  code_to_execute <-  "x <- 4+4" #"df <- read.table(\"clipboard\", sep = \"\\t\", header = TRUE)"
+  code_to_execute <-  "x <- 4+4" 
   rstudioapi::sendToConsole(code = code_to_execute, execute = TRUE, echo = TRUE)
 }
